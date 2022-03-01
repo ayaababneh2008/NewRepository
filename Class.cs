a624fee9b9cd4b028fd3ddcd051765d63b6b6c70
Aya Ababneh <AAbabneh@eSenseSoftware.com>
2022-03-01 12:41:45 +0300

    Commit from master

--- Class.cs
+++ Class.cs
@@ -2,10 +2,12 @@
 
 public class Class1
 {
-	public Class1()
-	{
-		//Comment From Master branch
+    public Class1()
+    {
+        //Comment From Master branch
 
-		// This Comment from Another branch 
-	}
+        // This Comment from Another branch 
+    }
+
+    public task = 'from master to check the cjanges reflect';
 }
